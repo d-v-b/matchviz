cce89067d9885247fc89bc9d06bc6250558da5c7
Davis Bennett <davis.v.bennett@gmail.com>
2024-06-03 16:04:22 +0200

    initial support for parsing matches data

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -1,4 +1,4 @@
-from matchviz import create_neuroglancer_state, save_points_tile, save_interest_points, image_name_to_tile_coord
+from matchviz import create_neuroglancer_state, get_tile_coords, load_points, parse_bigstitcher_xml_from_s3, parse_idmap, save_points_tile, save_interest_points, image_name_to_tile_coord
 import pytest
 
 @pytest.mark.skip
@@ -22,12 +22,13 @@ def test_viz(tmpdir):
         out_prefix=str(tmpdir))
     
 def test_save_points_tile():
+    bs_url = 's3://aind-open-data/exaSPIM_708373_2024-04-02_19-49-38_alignment_2024-05-07_18-15-25/'
+    bs_model = parse_bigstitcher_xml_from_s3(bs_url)
     tile_name = 'tile_x_0000_y_0000_z_0000_ch_488'
-    image_url = 's3://aind-open-data/exaSPIM_708373_2024-04-02_19-49-38_flatfield-correction_2024-04-15_18-15-40/SPIM.ome.zarr/tile_x_0000_y_0000_z_0000_ch_488.zarr'
-    alignment_url = 's3://aind-open-data/exaSPIM_708373_2024-04-02_19-49-38_alignment_2024-05-07_18-15-25/interestpoints.n5/tpId_0_viewSetupId_0/beads'
+    alignment_url = 's3://aind-open-data/exaSPIM_708373_2024-04-02_19-49-38_alignment_2024-05-07_18-15-25/interestpoints.n5/tpId_0_viewSetupId_0/'
     out_prefix = 'points'
-
-    save_points_tile(tile_name=tile_name, image_url=image_url, alignment_url=alignment_url, out_prefix=out_prefix)
+    tile_coords = get_tile_coords(bs_model)
+    save_points_tile(vs_id = 0, tile_name=tile_name, alignment_url=alignment_url, out_prefix=out_prefix, tile_coords=tile_coords)
 
 @pytest.mark.parametrize('x', (0, 1))
 @pytest.mark.parametrize('y', (0, 1))
@@ -36,3 +37,12 @@ def test_save_points_tile():
 def test_image_name_to_coordinate(x, y, z, ch):
     image_name = f'tile_x_{x:04}_y_{y:04}_z_{z:04}_ch_{ch}.zarr'
     assert image_name_to_tile_coord(image_name) == {'x': x, 'y': y, 'z': z, 'ch' : ch}
+
+def test_parse_idmap():
+    data = {'0,1,beads': 0, '0,3,beads': 1}
+    assert parse_idmap(data) == {(0, 1, 'beads'): 0, (0, 3, 'beads'): 1}
+
+@pytest.mark.skip
+def test_load_points():
+    url = 's3://aind-open-data/exaSPIM_708373_2024-04-02_19-49-38_alignment_2024-05-07_18-15-25/interestpoints.n5/tpId_0_viewSetupId_3/beads/'
+    table = load_points(url)
