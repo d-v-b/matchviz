a244e6c006bfaecdd34af1762e59a25593c5ecbf
Davis Bennett <davis.v.bennett@gmail.com>
2024-07-15 15:45:15 +0200

    adjust neuroglancer json cli

--- src/matchviz/cli.py
+++ src/matchviz/cli.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import os
 import json
+from typing import Sequence
 import click
 import fsspec
 import logging
@@ -22,67 +23,31 @@ def cli(): ...
 
 
 @cli.command("save-points")
-@click.option("--src", type=click.STRING)
-@click.option("--dest", type=click.STRING)
-@click.option("--ngjson", type=click.STRING)
-@click.option("--nghost", type=click.STRING)
-def save_interest_points_cli(
-    src: str | None, dest: str | None, ngjson: str | None, nghost: str | None
-):
-    if src is None:
-        raise ValueError(
-            "A source (i.e., a path or URL to a alignment directory) must be provided."
-        )
-
-    if dest is None:
-        raise ValueError(
-            "A dest (i.e., a path to a URL or directory) must be provided."
-        )
-
+@click.option("--src", type=click.STRING, required=True)
+@click.option("--dest", type=click.STRING, required=True)
+def save_interest_points_cli(src: str, dest: str):
     logging.basicConfig(level="INFO")
-    save_points(url=src, dest=dest, ngjson=ngjson, nghost=nghost)
+    save_points(url=src, dest=dest)
 
 
-def save_points(url: str, dest: str, ngjson: str | None, nghost: str | None):
+def save_points(url: str, dest: str):
     bs_model = parse_bigstitcher_xml_from_s3(url)
     save_interest_points(bs_model=bs_model, base_url=url, out_prefix=dest)
 
-    if ngjson is not None:
-        if nghost is not None:
-            ng_url = os.path.join(nghost, dest)
-        else:
-            ng_url = dest
-        tilegroup_s3_url = get_tilegroup_s3_url(bs_model)
-
-        fs, _ = fsspec.url_to_fs(ngjson)
-
-        if not ngjson.startswith("s3://"):
-            if nghost is None:
-                raise ValueError(
-                    "You must provide a hostname to generate a neuroglancer viewer state if you are saving to local storage"
-                )
-
-        for style in neuroglancer_view_styles:
-            state = create_neuroglancer_state(
-                image_url=tilegroup_s3_url, points_url=ng_url, style=style
-            )
-            with fs.open(ngjson, mode="w") as fh:
-                fh.write(json.dumps(state.to_json()))
-
 
 @cli.command("ngjson")
-@click.argument("alignment_url", type=click.STRING)
-@click.argument("points_url", type=click.STRING)
-@click.argument("dest_path", type=click.STRING)
+@click.option("--alignment-url", type=click.STRING, required=True)
+@click.option("--points-url", type=click.STRING, required=True)
+@click.option("--dest-path", type=click.STRING, required=True)
 @click.option("--style", type=click.STRING, multiple=True)
 def save_neuroglancer_json_cli(
     alignment_url: str,
     dest_path: str,
     points_url: str,
-    style: list[NeuroglancerViewerStyle] | None = None,
+    style: Sequence[NeuroglancerViewerStyle] | None = None,
 ):
     if style is None or len(style) < 1:
-        style = ["images_combined", "images_split"]
+        style = neuroglancer_view_styles
     for _style in style:
         save_neuroglancer_json(
             alignment_url=alignment_url,
@@ -106,7 +71,10 @@ def save_neuroglancer_json(
     )
     out_fname = f"{style}.json"
     out_path = os.path.join(dest_path, out_fname)
-    fs, _ = fsspec.url_to_fs(dest_path)
+    if dest_path.startswith("s3://"):
+        fs, _ = fsspec.url_to_fs(dest_path)
+    else:
+        fs, _ = fsspec.url_to_fs(dest_path, auto_mkdir=True)
 
     with fs.open(out_path, mode="w") as fh:
         fh.write(json.dumps(state.to_json()))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,7 +9,7 @@ import os
         "s3://aind-open-data/exaSPIM_708373_2024-04-02_19-49-38_alignment_2024-05-07_18-15-25/"
     ],
 )
-def test_cli(tmpdir, alignment_url):
+def test_save_points(tmpdir, alignment_url):
     out_path = os.path.join(str(tmpdir), "tile_alignment_visualization")
     run_result = subprocess.run(
         [
@@ -23,6 +23,30 @@ def test_cli(tmpdir, alignment_url):
             "test.json",
             "--nghost",
             "http://localhost:3000",
+        ]
+    )
+    assert run_result.returncode == 0
+
+
+@pytest.mark.parametrize(
+    "alignment_url",
+    [
+        "s3://aind-open-data/exaSPIM_708373_2024-04-02_19-49-38_alignment_2024-05-07_18-15-25/"
+    ],
+)
+def test_save_neuroglancer(tmpdir, alignment_url):
+    points_url = os.path.join(alignment_url, "tile_alignment_visualization", "points")
+    out_path = os.path.join(str(tmpdir), "tile_alignment_visualization", "neuroglancer")
+    run_result = subprocess.run(
+        [
+            "matchviz",
+            "ngjson",
+            "--alignment-url",
+            alignment_url,
+            "--points-url",
+            points_url,
+            "--dest-path",
+            out_path,
         ]
     )
     assert run_result.returncode == 0
